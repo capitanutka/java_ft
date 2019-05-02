0fd298efa8d20123f26d8b014baed9b4513d62e8
capitanutka <38263514+capitanutka@users.noreply.github.com>
2019-05-02 20:38:54 +0300

    Added first step

---
+++ sandbox/src/test/java/by/cherdakk/PointTest.java
@@ -0,0 +1,18 @@
+package by.cherdakk;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static by.cherdakk.MyFirstProgramm.distance;
+
+public class PointTest {
+
+  @Test
+  public void testDistance() {
+    Point p1 = new Point(3,7.2);
+    Point p2 = new Point(3,4.2);
+    Point p3 = new Point(8,7.2);
+    Assert.assertEquals(distance(p1,p2), 3.0);
+    Assert.assertEquals(distance(p1,p3), 5.0);
+  }
+}

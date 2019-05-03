0978c3f251f761f542ee639a37ae717830f8e75a
capitanutka <38263514+capitanutka@users.noreply.github.com>
2019-05-03 17:59:02 +0300

    class GroupData added

--- addressbook-web-tests/src/test/java/by/cherdakk/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/by/cherdakk/GroupCreationTest.java
@@ -28,7 +28,7 @@ public class GroupCreationTest {
   public void testGroupCreation() throws Exception {
     goToGroupPage();
     initGroupCreation();
-    fillGroupForm("test1", "test2", "test3");
+    fillGroupForm(new GroupData("test1", "test2", "test3"));
     submitGroupCreation();
     returnToGroupPage();
   }
@@ -41,13 +41,13 @@ public class GroupCreationTest {
     wd.findElement(By.name("submit")).click();
   }
 
-  private void fillGroupForm(String name, String header, String footer) {
+  private void fillGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(name);
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(header);
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(footer);
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
   }
 
   private void initGroupCreation() {
